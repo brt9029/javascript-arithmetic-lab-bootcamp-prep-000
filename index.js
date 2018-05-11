260bb249fdceb5ef6873811d4162004769af6780
Bryant Torres <brt9029@gmail.com>
2018-05-11 01:53:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,16 +23,17 @@ console.log(number)
 divideBy3()
 console.log(number)
 
-function inc(n){
-  
-}
-
-function dec(n){
-  
-}
-
 parseInt('2', 10)
 parseInt('2.2222', 10)
 parseInt('nonsense!', 10)
 
 parseFloat('80.123999')
+
+
+function inc(n){
+  n ++
+}
+
+function dec(n){
+  
+}
