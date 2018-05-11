0f7c4021ff7866c5d56e68eca469e98d01132a3b
Bryant Torres <brt9029@gmail.com>
2018-05-11 01:47:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var number = 10
+
+function add5(){
+  number += 5
+}
+function divideBy3(){
+  number /= 3
+}
+
+divideBy3()
+console.log(number)
+// reset number
+number = 10
+
+add5()
+console.log(number)
+
+divideBy3()
+console.log(number)
+
+function inc(n){
+  
+}
+
+function dec(n){
+  
+}
